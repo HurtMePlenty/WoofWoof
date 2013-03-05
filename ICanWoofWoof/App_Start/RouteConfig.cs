276820af200058027d7bd0857cc42162300022d9
unknown <ASemenov@ASEMENOV.viacode.com>
2013-03-05 16:23:46 +0400

    First

---
+++ ICanWoofWoof/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ICanWoofWoof
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+            name: "Modernizer",
+            url: "Modern",
+            defaults: new { controller = "Home", action = "CheckModernizer" }
+        );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ ICanWoofWoof/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ICanWoofWoof.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShowAll()
+        {
+            return View(new string[] {"value1", "value2"});
+        }
+
+        public ActionResult CheckModernizer()
+        {
+            return View();
+        }
+
+        public ActionResult CheckKnockout()
+        {
+            return View();
+        }
+
+    }
+}

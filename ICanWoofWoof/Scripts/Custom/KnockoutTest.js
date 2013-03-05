b33967a8016bf962bbad01301226127e8851b81e
HurtMePlenty <clockerzzz@rambler.ru>
2013-03-05 22:17:12 +0400

    4

--- ICanWoofWoof/Scripts/Custom/KnockoutTest.js
+++ ICanWoofWoof/Scripts/Custom/KnockoutTest.js
@@ -3,19 +3,24 @@
     $('.knockout-container-collapser').mousedown(function (e) {
         e.preventDefault();
     });
-    $('.knockout-container').css('height', 500);
+
     $('.knockout-container-collapser').click(function (e) {
         e.preventDefault();
         var $cont = $(this).parent().find('.knockout-container');
-        var isCollapsed = $cont.data('isCollapsed');
-        if(!isCollapsed) {
-            $cont.data('isCollapsed', true);
-            $cont.data('initHeight', $cont.height());
-            $cont.css('height', 0);
-        } else {
-            $cont.data('isCollapsed', false);
-            $cont.css('height', $cont.data('initHeight'));
-        }
+        $cont.removeClass("collapse-transitions");
+        $cont.css('height', $cont.height());
+        window.setTimeout(function () {
+            $cont.addClass("collapse-transitions");
+            var isCollapsed = $cont.data('isCollapsed');
+            if (!isCollapsed) {
+                $cont.data('isCollapsed', true);
+                $cont.data('initHeight', $cont.height());
+                $cont.css('height', 0);
+            } else {
+                $cont.data('isCollapsed', false);
+                $cont.css('height', $cont.data('initHeight'));
+            }
+        }, 10);
     });
 
 

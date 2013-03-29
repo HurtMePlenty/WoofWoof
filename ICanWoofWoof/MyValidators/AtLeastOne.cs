8aed6fcc73e3813e74433170a7b325bcfe681b0b
HurtMePlenty <clockerzzz@rambler.ru>
2013-03-29 21:57:51 +0400

    dfg

--- ICanWoofWoof/MyValidators/AtLeastOne.cs
+++ ICanWoofWoof/MyValidators/AtLeastOne.cs
@@ -7,27 +7,32 @@ using System.Web.Mvc;
 
 namespace ICanWoofWoof.MyValidators
 {
-    public class AtLeastOne: ValidationAttribute, IClientValidatable
+    public class AtLeastOne : ValidationAttribute, IClientValidatable
     {
-        private const string defaultErrorMessage = "{0} or {1} is required.";
-        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
+        private const string DefaultErrorMessage = "{0} or {1} is required.";
+        private const string ValidationType = "atleastonerequired";
+
+        public AtLeastOne()
         {
-            yield return new ModelClientValidationRule()
-                {
-                    ErrorMessage = string.Format(defaultErrorMessage, metadata.DisplayName, string.Empty),
-                    ValidationType = "atleastonerequired"
-                };
-        
+            var test = GetClientValidationRules(null,null);
+        }
+
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata,
+                                                                               ControllerContext context)
+        {
+            var mcvRule = new ModelClientValidationRule
+                              {
+                                  ErrorMessage = string.Format(DefaultErrorMessage, "test", string.Empty),
+                                  ValidationType = ValidationType
+                              };
+            mcvRule.ValidationParameters["param"] = "value";
+            yield return mcvRule;
+
         }
 
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             return base.IsValid(value, validationContext);
-        }
-
-        class ModelClientValidationAtLeastOneRule: ModelClientValidationRule
-        {
-            
         }
     }
 }

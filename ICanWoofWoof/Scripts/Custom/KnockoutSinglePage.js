b7069bd7110c622acc3bc200948df2cb5d8a68ef
HurmMePlenty <clockerzzz@rambler.ru>
2013-04-08 15:10:33 +0300

    sdf

--- ICanWoofWoof/Scripts/Custom/KnockoutSinglePage.js
+++ ICanWoofWoof/Scripts/Custom/KnockoutSinglePage.js
@@ -5,12 +5,19 @@
         self.folders = ['Inbox', 'Archive', 'Sent', 'Spam'];
         self.chosenFolderId = ko.observable();
         self.folderData = ko.observable();
+        self.chosenMailData = ko.observable(null);
         self.tabClass = function (tab) {
             return self.chosenFolderId() == tab ? 'active-tab' : '';
         };
         self.goToFolder = function (folder) {
             self.chosenFolderId(folder);
+            self.chosenMailData(null);
             $.get('/api/mailapi/' + folder, self.folderData);
+        };
+
+        self.selectMail = function(mail) {
+            self.chosenMailData(mail);
+            self.chosenFolderId(null);
         };
     }
 

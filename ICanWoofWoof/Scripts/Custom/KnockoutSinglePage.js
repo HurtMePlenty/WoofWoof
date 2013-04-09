c4036d1100192951d89bd6d6c35ef6c6472b2c06
HurmMePlenty <clockerzzz@rambler.ru>
2013-04-09 05:45:44 +0300

    sammy.js added

--- ICanWoofWoof/Scripts/Custom/KnockoutSinglePage.js
+++ ICanWoofWoof/Scripts/Custom/KnockoutSinglePage.js
@@ -4,21 +4,35 @@
         var self = this;
         self.folders = ['Inbox', 'Archive', 'Sent', 'Spam'];
         self.chosenFolderId = ko.observable();
-        self.folderData = ko.observable();
+        self.folderData = ko.observable(null);
         self.chosenMailData = ko.observable(null);
         self.tabClass = function (tab) {
             return self.chosenFolderId() == tab ? 'active-tab' : '';
         };
         self.goToFolder = function (folder) {
-            self.chosenFolderId(folder);
-            self.chosenMailData(null);
-            $.get('/api/mailapi/' + folder, self.folderData);
+            location.hash = folder;
         };
 
         self.selectMail = function(mail) {
             self.chosenMailData(mail);
             self.chosenFolderId(null);
         };
+
+        Sammy(function() {
+            this.get('#:folder', function() {
+                self.chosenFolderId(this.params.folder);
+                self.chosenMailData(null);
+                $.get('/api/mailapi/' + this.params.folder, self.folderData);
+            });
+
+            this.get('#:folder/', function() {
+
+            });
+
+            this.get('', function() {
+                this.app.runRoute('get', '#Inbox');
+            });
+        }).run();
     }
 
     ko.applyBindings(new WebmailViewModel());
